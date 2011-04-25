57eac92d33f604ea8ee544374d0099f5cd8e8ac7
Michael Grüner <michael.gruner@ridgerun.com>
2011-04-25 15:23:19 -0600

    Added function declarations and error codes

--- src/driver.h
+++ src/driver.h
@@ -1,8 +1,25 @@
 #ifndef __DRIVER_H__
 #define __DRIVER_H__
 
-extern int usbmain(void);
 
+
+typedef enum _GADGET_EXIT_CODE
+{
+  /** Everything OK */	
+  EOK,	
+  
+  /** Error changing to gadget directory */
+  ERR_GAD_DIR,
+  
+  /** Error initializing gadget device */
+  ERR_INI_DEV
+  
+  	
+} GADGET_EXIT_CODE;
+
+extern int usb_gadget_new(void);
+
+extern int usb_gadget_free(void);
 
 
 #endif /* __DRIVER_H__ */
